aa1cb419a2468f891a3a7e65b0aacf18c1649b4d
Qoquan <boulangerloic1@gmail.com>
2026-03-14 09:56:13 +0100

    final

--- Library.Tests/obj/Debug/net9.0/Library.Tests.AssemblyInfo.cs
+++ Library.Tests/obj/Debug/net9.0/Library.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99a369ccb9a9ded6a2f8854518c0fa5cd3b6b2cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8103a5393e4bc1d47e3f0ec41b53cb5a53f2e7d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
